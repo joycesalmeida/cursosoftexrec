6675b464454059b91f07476b66b312fe55bc8302
Joyce Almeida <105922645+joycesalmeida@users.noreply.github.com>
2022-08-09 14:34:20 -0300

    Update PROJETO 09 (cronometro).py

--- PROJETO 09 (cronometro).py
+++ PROJETO 09 (cronometro).py
@@ -1,8 +1,8 @@
 import time
 
 def countdown(num_of_secs):
-    num_of_secs = int(num_of_secs):
-    while num_of_secs != 0
+    num_of_secs = int(num_of_secs)
+    while num_of_secs != 0:
         m, s = divmod(num_of_secs, 60)
         min_sec_format = '{:02d}:{:02d}'.format(m, s)
         print(min_sec_format, end='\n')
